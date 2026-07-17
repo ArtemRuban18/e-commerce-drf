a80cff0950d318a09b4c6c41fac2f6087dae0dc4
Artem Ruban <artemruban762@gmail.com>
2026-07-17 17:01:26 +0200

    Add type annotation

--- apps/products/services.py
+++ apps/products/services.py
@@ -1,9 +1,10 @@
 from .models import Category, Product
 from django.shortcuts import get_object_or_404
+from django.db.models import QuerySet
 
 class CategoryService:
     @staticmethod
-    def get_category_tree(category_id):
+    def get_category_tree(category_id: int) -> list[int]:
         category = get_object_or_404(Category, id = category_id)
         category_list = [category.id]
         children = category.children.all()
@@ -13,7 +14,7 @@ class CategoryService:
 
 class ProductService:
     @staticmethod
-    def get_products_by_category(category_id):
+    def get_products_by_category(category_id:int) -> QuerySet[Product]:
         category_id = CategoryService.get_category_tree(category_id)
         products = Product.objects.filter(category__id__in = category_id)
         return products
